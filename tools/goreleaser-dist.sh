24b72f22617fbe8d8c88719c11b6eebbc8a4d2ed
Dustin Blackman <dev@dustinblackman.com>
2022-11-19 09:22:45 -0500

    feat: add m1 builds

--- tools/goreleaser-dist.sh
+++ tools/goreleaser-dist.sh
@@ -7,6 +7,8 @@ target="$1"
 goTargetToRust() {
 	if [[ "$target" == "darwin_amd64_v1" ]]; then
 		echo "x86_64-apple-darwin"
+	elif [[ "$target" == "darwin_arm64" ]]; then
+		echo "aarch64-apple-darwin"
 	elif [[ "$target" == "linux_amd64_v1" ]]; then
 		echo "x86_64-unknown-linux-gnu"
 	elif [[ "$target" == "linux_arm64" ]]; then
